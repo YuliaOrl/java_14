b996fa92d9418c729dc6ea5e83480a6fe6917922
YuliaOrl <orbitalik32@gmail.com>
2022-07-29 18:57:47 +0300

    first commit

--- src/main/java/guru/qa/Ticket.java
+++ src/main/java/guru/qa/Ticket.java
@@ -1,2 +1,16 @@
-package guru.qa;public class Ticket {
+package guru.qa;
+
+public class Ticket {
+
+    public static void main(String[] args) {
+        User human_1 = new User("Юлия", "женский", 39);
+        human_1.getGift();
+        User human_2 = new User("Саша", "мужской", 40);
+        human_2.getGift();
+        User human_3 = new User("Дмитрий", "мужской", 7);
+        human_3.getGift();
+        User human_4 = new User("Яна", "женский", 13);
+        human_4.getGift();
+    }
+
 }
